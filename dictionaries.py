c904b50872e5012c33a49de6193c705d8486e315
Radoslaw Kuzma <rdkkzm@gmail.com>
2019-10-09 14:33:51 +0200

    added test measurements

--- dictionaries.py
+++ dictionaries.py
@@ -1,39 +1,30 @@
 import timeit
 
-# num, rep = 1_000, 3
 
-# def a():
-#     # l = range(50, -50, -2)
-#     l = range(10_000)
-#     d = {}
-#     for p, v in enumerate(l):
-#         d[p] = v
-#     return d
-# # print(min(timeit.repeat(a, number=num, repeat=rep)))
-#
-# def b():
-#     # l = range(50, -50, -2)
-#     l = range(10_000)
-#     return {p: v for p, v in enumerate(l)}
-# # print(min(timeit.repeat(b, number=num, repeat=rep)))
-
-num, rep = 1_000, 3
 def senior_dev():
     return dict(enumerate(range(10_000)))
-print('senior_dev >> ', min(timeit.repeat(senior_dev, number=num, repeat=rep)))
 
 def medium_dev():
     return {p: v for p, v in enumerate(range(10_000))}
-print('medium_dev >> ', min(timeit.repeat(medium_dev, number=num, repeat=rep)))
 
 def junior_dev():
     dictio = {}
     for k, v in enumerate(range(10_000)):
         dictio[k] = v
     return dictio
+
+num, rep = 1_000, 3
+print('senior_dev >> ', min(timeit.repeat(senior_dev, number=num, repeat=rep)))
+print('medium_dev >> ', min(timeit.repeat(medium_dev, number=num, repeat=rep)))
 print('junior_dev >> ', min(timeit.repeat(junior_dev, number=num, repeat=rep)))
 
 
 
+'''
+num, rep = 1_000, 3
 
+senior_dev >>  0.6572029499802738
+medium_dev >>  0.7706875120056793
+junior_dev >>  0.8078349230345339
+'''
 
