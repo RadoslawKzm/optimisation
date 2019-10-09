7f9773b55c9ed384b911bfba3bcb3721d70202c7
Radoslaw Kuzma <rdkkzm@gmail.com>
2019-10-09 14:17:48 +0200

    made 3 dicting test b2win string+string, string.format and fstring. Fstring won, is fastest and has best clearity

--- string_formatting.py
+++ string_formatting.py
@@ -1,10 +1,25 @@
 import timeit
-def a():
+def string_arithmetic_test():
     s = "World"
     return "Hello, " + s + "!"
-print(min(timeit.repeat(a, number=100_000, repeat=100)))
 
-def b():
+def format_string_test():
+    s = "World"
+    return "Hello, {}!".format(s)
+
+def f_string_test():
     s = "World"
     return f"Hello, {s}!"
-print(min(timeit.repeat(b, number=100_000, repeat=100)))
+
+num, rep = 1_000_000, 100
+print('string "+" string >> ', min(timeit.repeat(string_arithmetic_test, number=num, repeat=rep)))
+print('string.format >> ', min(timeit.repeat(format_string_test, number=num, repeat=rep)))
+print('fstring >> ', min(timeit.repeat(f_string_test, number=num, repeat=rep)))
+
+'''
+num, rep = 1_000_000, 100
+
+string "+" string >>  0.1584004471078515
+string.format >>  0.2886155489832163
+fstring >>  0.13300530705600977
+'''
