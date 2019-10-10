1d2f046282359b3d1de7585fc498d83e333f49cf
Radoslaw Kuzma <rdkkzm@gmail.com>
2019-10-10 11:56:50 +0200

    added fstring for better visibility

--- dictionaries.py
+++ dictionaries.py
@@ -1,22 +1,25 @@
 import timeit
 
 
-def senior_dev():
-    return dict(enumerate(range(10_000)))
+def dictio_test():
+    rang = 10_000
+    def senior_dev():
+        return dict(enumerate(range(rang)))
 
-def medium_dev():
-    return {p: v for p, v in enumerate(range(10_000))}
+    def medium_dev():
+        return {p: v for p, v in enumerate(range(rang))}
 
-def junior_dev():
-    dictio = {}
-    for k, v in enumerate(range(10_000)):
-        dictio[k] = v
-    return dictio
+    def junior_dev():
+        dictio = {}
+        for k, v in enumerate(range(rang)):
+            dictio[k] = v
+        return dictio
 
-num, rep = 1_000, 3
-print('senior_dev >> ', min(timeit.repeat(senior_dev, number=num, repeat=rep)))
-print('medium_dev >> ', min(timeit.repeat(medium_dev, number=num, repeat=rep)))
-print('junior_dev >> ', min(timeit.repeat(junior_dev, number=num, repeat=rep)))
+    num, rep = 1_000, 3
+    print(f'range = {rang:_}, num, rep = {num:_}, {rep:_}\n')
+    print('senior_dev >> ', min(timeit.repeat(senior_dev, number=num, repeat=rep)))
+    print('medium_dev >> ', min(timeit.repeat(medium_dev, number=num, repeat=rep)))
+    print('junior_dev >> ', min(timeit.repeat(junior_dev, number=num, repeat=rep)))
 
 
 
