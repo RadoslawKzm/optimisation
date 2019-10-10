3fc12fb136626b66aa0c7cfd555279d66c37056d
Radoslaw Kuzma <rdkkzm@gmail.com>
2019-10-10 11:54:20 +0200

    added fstring for better visibility

--- lists.py
+++ lists.py
@@ -2,39 +2,34 @@ from collections import Counter
 import timeit
 
 
-
 def not_common_numbers_test():
-    lst1 = list(range(10_000_000))
+    rang = 10_000_000
+    lst1 = list(range(rang))
     lst2 = list(range(1_000_000))
-    num, rep = 1, 10
 
     def counter_test():
         cnt1 = Counter(lst1)
         cnt2 = Counter(lst2)
         cnt3 = cnt1 - cnt2
         return list(cnt3.keys())
-    print(f'counter_test >> {min(timeit.repeat(counter_test, number=num, repeat=rep)):0.4f}s')
 
     def counter_subtract_test():
         cnt1 = Counter(lst1)
         cnt2 = Counter(lst2)
         cnt1.subtract(cnt2)
         return list(+cnt1)
-    print(f'counter subtract test >> {min(timeit.repeat(counter_subtract_test, number=num, repeat=rep)):0.4f}s')
 
     def set_diference_test():
         st1 = set(lst1)
         st2 = set(lst2)
         st3 = st1.difference(st2)
         return list(st3)
-    print(f'set_diference_test >> {min(timeit.repeat(set_diference_test, number=num, repeat=rep)):0.4f}s')
 
     def set_minus_test():
         st1 = set(lst1)
         st2 = set(lst2)
         st3 = st1 - st2
         return list(st3)
-    print(f'set_minus_test >> {min(timeit.repeat(set_minus_test, number=num, repeat=rep)):0.4f}s')
 
     def for_i_in_list_test():
         lst3 = []
@@ -42,7 +37,15 @@ def not_common_numbers_test():
             if item not in lst2:
                 lst3.append(item)
         return lst3
+
+    num, rep = 1, 10
+    print(f'range = {rang:_}, num, rep = {num:_}, {rep:_}')
+    print(f'counter_test >> {min(timeit.repeat(counter_test, number=num, repeat=rep)):0.4f}s')
+    print(f'counter subtract test >> {min(timeit.repeat(counter_subtract_test, number=num, repeat=rep)):0.4f}s')
+    print(f'set_diference_test >> {min(timeit.repeat(set_diference_test, number=num, repeat=rep)):0.4f}s')
+    print(f'set_minus_test >> {min(timeit.repeat(set_minus_test, number=num, repeat=rep)):0.4f}s')
     # print(f'for_i_in_list_test >> {min(timeit.repeat(for_i_in_list_test, number=num, repeat=rep)):0.4f}s') # dont use it works for small data det
+
 '''
 lst1 = list(range(10_000_000))
 lst2 = list(range(1_000_000))
@@ -53,6 +56,7 @@ counter subtract test >> 2.7646s
 set_diference_test >> 0.8165s
 set_minus_test >> 0.8166s
 '''
+
 
 def list_making_test():
     rang = 100_000
