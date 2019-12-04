e734abfbfb97b9f4d607005491a3bbab8fc607c1
Radoslaw Kuzma <rdkkzm@gmail.com>
2019-12-04 10:47:16 +0100

    done case try vs if, if winning till 82% ratio of true/exception

--- try_vs_if/try_vs_if.py
+++ try_vs_if/try_vs_if.py
@@ -1,9 +1,17 @@
 import timeit
+import matplotlib
+import numpy as np
+matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
 def letter_gen(rang:int):
     for i in range(rang):
         yield 'a'
+
+def float_gen(start, stop, step):
+    for number in np.arange(start=start, stop=stop, step=step):
+        yield number
+
 
 def try_test():
     for i in range(rang_true):
@@ -34,16 +42,17 @@ def test_suite():
     num, rep = 1, 1
     try_test_result = min(timeit.repeat(try_test, number=num, repeat=rep))
     if_test_result = min(timeit.repeat(if_test, number=num, repeat=rep))
-    return (try_test_result, if_test_result)
+    return try_test_result, if_test_result
 
-def looping_func():
+def looping_func(start, end, step):
     global rang_true
     global rang_false
     results_try = {}
     results_if = {}
-    for i in range(1,101):
+    for i in float_gen(start, end, step):
+        print(f'percent processing >> {i:0.4f}%')
         percent = i  # percent of true occrances
-        rang_true = 10_000
+        rang_true = 1_000_000
         rang_false = int(rang_true * (100 / percent) - rang_true)
         results = test_suite()
         results_try[i] = results[0]
@@ -51,8 +60,10 @@ def looping_func():
     return results_try, results_if
 
 
+start, stop, step = 80, 100, 0.1
+stop = stop+step
 
-dictio = looping_func()
+dictio = looping_func(start,stop,step)
 try_results = dictio[0]
 if_results = dictio[1]
 
@@ -68,5 +79,5 @@ plt.legend()
 
 
 '''
-
+try looses with if till about 82% of true/error ratio, after 82% try is getting significant advantages
 '''
