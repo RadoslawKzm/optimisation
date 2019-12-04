75196220a5ecd091f8074b12764bcccd8e756698
Radoslaw Kuzma <rdkkzm@gmail.com>
2019-12-04 11:38:51 +0100

    updated case with %s comparsion

--- string_formatting.py
+++ string_formatting.py
@@ -1,4 +1,13 @@
 import timeit
+
+def f_string_test():
+    s = "World"
+    return f"Hello, {s}!"
+
+def percent_string_test():
+    s = "World"
+    return "Hello, %s!" %(s)
+
 def string_arithmetic_test():
     s = "World"
     return "Hello, " + s + "!"
@@ -7,21 +16,20 @@ def format_string_test():
     s = "World"
     return "Hello, {}!".format(s)
 
-def f_string_test():
-    s = "World"
-    return f"Hello, {s}!"
 
 num, rep = 1_000_000, 100
 print(f'num, rep = {num:_}, {rep:_}\n')
 print(f'string "+" string >> {min(timeit.repeat(string_arithmetic_test, number=num, repeat=rep)):0.4f}s')
+print(f'string %s >> {min(timeit.repeat(percent_string_test, number=num, repeat=rep)):0.4f}s')
 print(f'string.format >> {min(timeit.repeat(format_string_test, number=num, repeat=rep)):0.4f}s')
 print(f'fstring >> {min(timeit.repeat(f_string_test, number=num, repeat=rep)):0.4f}s')
 
 '''
 num, rep = 1_000_000, 100
 
-string "+" string >> 0.1619s
-string.format >> 0.2984s
-fstring >> 0.1339s
+fstring >> 0.1333s
+string "+" string >> 0.1606s
+string %s >> 0.2458s
+string.format >> 0.3012s
 
 '''
